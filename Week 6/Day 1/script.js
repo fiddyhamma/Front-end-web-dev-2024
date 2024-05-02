b7709d3f7101c247c4bc431d388ae07e818eb646
Peace Daniel <peacedaniel612@gmail.com>
2024-05-02 09:43:58 +0100

    peacedanielmonkeytask

--- Week 6/Day 1/script.js
+++ Week 6/Day 1/script.js
@@ -1,4 +1,27 @@
-const closedFace = document.querySelector('.closed');
-const openFace = document.querySelector('.open');
+const closedFace = document.querySelector('.emoji_closed_active');
+const openFace = document.querySelector('.emoji_open');
 
 // Add event listener here
+closedFace.addEventListener("click", () => {
+    openFace.style.display = "block";
+    closedFace.style.display = "none";
+  });
+
+openFace.addEventListener("click", () => {
+    closedFace.style.display ="block";
+    openFace.style.display = "none";
+})
+
+// Using jquery
+$(document).ready(function(){
+    $(".emoji_closed_active").click(function(){
+        $(".emoji_closed_active").hide();
+        $(".emoji_open").show();
+      });
+
+    $(".emoji_open").click(function(){
+      $(".emoji_open").hide();
+      $(".emoji_closed_active").show();
+    });
+   
+  });
